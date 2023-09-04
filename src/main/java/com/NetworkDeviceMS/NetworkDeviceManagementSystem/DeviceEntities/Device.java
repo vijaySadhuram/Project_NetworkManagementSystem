c070e187b524de9f07251ea0d4ce26a334b39843
Vijay <vijaysadhuram4868@gmail.com>
2023-09-04 18:39:37 +0530

    added implementation

--- src/main/java/com/NetworkDeviceMS/NetworkDeviceManagementSystem/DeviceEntities/Device.java
+++ src/main/java/com/NetworkDeviceMS/NetworkDeviceManagementSystem/DeviceEntities/Device.java
@@ -6,5 +6,55 @@ public class Device {
     private String name;
     private String version;
     private String brand;
+	/**
+	 * @param id
+	 * @param name
+	 * @param version
+	 * @param brand
+	 */
+	public Device(Long id, String name, String version, String brand) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.version = version;
+		this.brand = brand;
+	}
+	/**
+	 * 
+	 */
+	public Device() {
+		super();
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getVersion() {
+		return version;
+	}
+	public void setVersion(String version) {
+		this.version = version;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	@Override
+	public String toString() {
+		return "Device [id=" + id + ", name=" + name + ", version=" + version + ", brand=" + brand + "]";
+	}
+	
+    
+    
 
 }
--- src/main/java/com/NetworkDeviceMS/NetworkDeviceManagementSystem/DeviceService/DeviceServiceimple.java
+++ src/main/java/com/NetworkDeviceMS/NetworkDeviceManagementSystem/DeviceService/DeviceServiceimple.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import com.NetworkDeviceMS.NetworkDeviceManagementSystem.DeviceEntities.Device;
 
-public interface DeviceService {
-	
-public List<Device> getDevice();
+public class DeviceServiceimple implements DeviceService {
+
+	@Override
+	public List<Device> getDevice() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
