3a1f2df67839c5990251b32c903920d48422f5f8
Vijay <vijaysadhuram4868@gmail.com>
2023-09-04 17:20:43 +0530

    testing the application

--- src/main/java/com/NetworkDeviceMS/NetworkDeviceManagementSystem/Controller/DeviceController.java
+++ src/main/java/com/NetworkDeviceMS/NetworkDeviceManagementSystem/Controller/DeviceController.java
@@ -1,5 +1,16 @@
 package com.NetworkDeviceMS.NetworkDeviceManagementSystem.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+//Controller Layer-->Handle the HTTP Request to the Service layer
+@RestController
 public class DeviceController {
+ //method mapping
+	@GetMapping("/home")
+	public String  Home() {
+		return "i ma vija";
+	}
+	
+	
 
 }
