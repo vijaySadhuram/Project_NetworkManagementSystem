3dde82476f365d8785b691720324474fd6369872
Vijay <vijaysadhuram4868@gmail.com>
2023-09-04 16:39:43 +0530

    project initialization

---
+++ src/main/java/com/NetworkDeviceMS/NetworkDeviceManagementSystem/NetworkDeviceManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.NetworkDeviceMS.NetworkDeviceManagementSystem;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkDeviceManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkDeviceManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NetworkDeviceMS/NetworkDeviceManagementSystem/NetworkDeviceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NetworkDeviceMS.NetworkDeviceManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkDeviceManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
