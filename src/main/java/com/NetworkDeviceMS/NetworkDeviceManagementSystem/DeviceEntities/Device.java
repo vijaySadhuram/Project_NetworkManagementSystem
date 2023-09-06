f6754fc52f6624f31d3bd966ce3c9ede24d019f4
Vijay <vijaysadhuram4868@gmail.com>
2023-09-06 09:56:44 +0530

    ID value Generator

--- src/main/java/com/NetworkDeviceMS/NetworkDeviceManagementSystem/DeviceEntities/Device.java
+++ src/main/java/com/NetworkDeviceMS/NetworkDeviceManagementSystem/DeviceEntities/Device.java
@@ -1,6 +1,5 @@
 package com.NetworkDeviceMS.NetworkDeviceManagementSystem.DeviceEntities;
 
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
