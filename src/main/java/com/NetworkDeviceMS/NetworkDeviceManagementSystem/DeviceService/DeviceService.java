c45f906bb852d586fc084de7bd43c2bc18ec247d
Vijay <vijaysadhuram4868@gmail.com>
2023-09-06 09:58:36 +0530

    Added All interface methods

--- src/main/java/com/NetworkDeviceMS/NetworkDeviceManagementSystem/DeviceService/DeviceService.java
+++ src/main/java/com/NetworkDeviceMS/NetworkDeviceManagementSystem/DeviceService/DeviceService.java
@@ -8,5 +8,7 @@ public interface DeviceService {
 	
 	 List<Device> getDevice();
 	 Device saveDevice(Device device);
+	 Device updateDevice(Long id, Device device);
+	 void deleteDevice(Long id);
 
 }
